e1f4e925972cf8cd063b06e04c4f3c3b89e5ebb2
NikolaevAnton <mjop41@gmail.com>
2022-02-01 21:51:49 +0300

    opacity реализовано не в слайдере

--- NikolaevAA_3.4/ContentView.swift
+++ NikolaevAA_3.4/ContentView.swift
@@ -9,24 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var value = 0.0
-    @State private var alpha = 0.4
+    @State private var currentValue = 0.0
+    @State private var targetValue = Int.random(in: 0...100)
+
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currentValue))
+        return 100 - difference
+    }
     
     var body: some View {
         VStack {
-            Text("Подвиньте слайдер как можно ближе к:")
+            Text("Подвиньте слайдер как можно ближе к: \(targetValue)")
                 .padding()
             HStack {
                 Text("0")
-                SliderToUIKitView(value: $value)
-                    .opacity(alpha)
+                SliderToUIKitView(
+                    currentValue: $currentValue)
+                    .opacity(Double(computeScore()) / 100)
                 Text("100")
             }.padding()
-            Button(action: {}) {
+            Button(action: {
+                print("\(computeScore())")
+            }) {
                 Text("проверить меня!")
             }
             .padding(.vertical, 20.0)
-            Button(action: {}) {
+            Button(action: {
+                currentValue = 0.0
+                targetValue = Int.random(in: 0...100)
+            }) {
                 Text("начать заново")
             }
         }
--- NikolaevAA_3.4/SliderToUIKitView.swift
+++ NikolaevAA_3.4/SliderToUIKitView.swift
@@ -8,39 +8,52 @@
 import SwiftUI
 
 struct SliderToUIKitView: UIViewRepresentable {
-    
-    @Binding var value: Double
-   
+    @Binding var currentValue: Double
     
     func makeUIView(context: Context) -> UISlider {
         let slider = UISlider()
         slider.minimumValue = 0
         slider.maximumValue = 100
-        slider.thumbTintColor = UIColor(.red)
-        
+        slider.thumbTintColor = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.changeValue(_:)),
+            for: .allTouchEvents)
         return slider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
-        uiView.value = Float(value)
+        uiView.value = Float(currentValue)
+    }
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(currentValue: $currentValue)
     }
     
     typealias UIViewType = UISlider
-    
-    
-    
-    
-    /*
-    var body: some View {
-        //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+}
+
+
+
+extension SliderToUIKitView {
+    class Coordinator: NSObject {
+        @Binding var currentValue: Double
+        
+        init(currentValue: Binding<Double>) {
+            self._currentValue = currentValue
+        }
+        
+        @objc func changeValue(_ sender: UISlider) {
+            currentValue = Double(sender.value)
+        }
     }
-     */
 }
 
 
 struct SliderToUIKitView_Previews: PreviewProvider {
     static var previews: some View {
-        SliderToUIKitView(value: .constant(10))
+        SliderToUIKitView(currentValue: .constant(10))
     }
 }
  
