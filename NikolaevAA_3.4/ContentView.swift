49b394d2797fd75f28541729556cac583fbfd8a8
NikolaevAnton <mjop41@gmail.com>
2022-02-01 14:03:53 +0300

    Initial Commit

---
+++ NikolaevAA_3.4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NikolaevAA_3.4
+//
+//  Created by Anton Nikolaev on 01.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NikolaevAA_3.4/NikolaevAA_3_4App.swift
@@ -0,0 +1,17 @@
+//
+//  NikolaevAA_3_4App.swift
+//  NikolaevAA_3.4
+//
+//  Created by Anton Nikolaev on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct NikolaevAA_3_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
